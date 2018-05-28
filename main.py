d271149f402e82f96876dc48b6dd9f62be915276
mbouhallel <m.bouhallel@gmail.com>
2018-05-28 16:59:09 +0200

    Connotation corrigée + ajout d'une condition d'assurance

--- main.py
+++ main.py
@@ -21,12 +21,13 @@ DIR = "./assets/sounds/"
 # GREETING_RESPONSES = ["'super mec", "mouai", "ça va, merci!", "Parfait"]
 
 liste_questions = ["Avez-vous aimé votre déjeuner?", "Le service était-il rapide?", "La quantité était-elle suffisante?"]
-liste_de_mots_clefs_pos = ["bien", "bon", "parfait", "impeccable", "super"]
-liste_de_mots_clefs_neg = ["mal", "terrible", "dégueulasse", "infecte"]
+liste_de_mots_clefs_pos = ["bien", "bon", "parfait", "impeccable", "super", "suffisant", "rapide"]
+liste_de_mots_clefs_neg = ["mal", "terrible", "dégueulasse", "infecte", "lent", "long"]
 negation = "pas"
-
+assurance = ["très","trop"]
 Jauge = compteur = 0
 last_val = 0
+connotation_finale = 0
 
 # def check_for_greeting(sentence):
 #     for word in sentence.words:
@@ -36,10 +37,7 @@ last_val = 0
 
 def listen():
     while 1:
-        #timeout = time.time() + 10
 
-        #if time.time() > timeout: #si pas de réponse de l'utilisateur en 10 secondes, le robot se reset
-        #break
         r = sr.Recognizer()
         try:
             sr.Microphone()
@@ -51,9 +49,6 @@ def listen():
                 recog = r.recognize_google(audio, language=LANG)
                 print(recog)
                 return recog
-
-                #person_name = get_name(recog)
-                #play_sounds_with_gtts("Salut, " + str(person_name))
 
         except Exception as err:
             time.sleep(1)
@@ -69,32 +64,26 @@ def play_sounds_with_gtts(m_text):
     while pygame.mixer.music.get_busy() == True:
         continue
 
-def get_name(sentence):
-    for sent in nltk.sent_tokenize(sentence):
-        for chunk in nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(sent))):
-            if hasattr(chunk, 'label'):
-                if chunk.label() == "PERSON":
-                    name = ' '.join(c[0] for c in chunk)
-                    return name
-
 def welcome_message():
-    hello_text = "Bonjour, appuyez sur une touche pour commencer"
+    hello_text = "Bonjour ! Appuyez sur une touche pour commencer"
     play_sounds_with_gtts(hello_text)
 
 def goodbye_message():
-    bye_text = "Merci d'avoir répondu. Au revoir"
+    bye_text = "Merci d'avoir répondu. Au revoir!"
     play_sounds_with_gtts(bye_text)
 
-def find_connotation():  #utiliser variable globale pour last_val. vérif si globlale
+def find_connotation():
     enreg = listen()
     compteur = val = val_inter = 0
-    while compteur < len(liste_de_mots_clefs_pos):
-        if enreg.find(liste_de_mots_clefs_pos[compteur]) > 0:
+    while compteur < len(liste_de_mots_clefs_pos): #Parcourir tous les mots-clé
+        if enreg.find(liste_de_mots_clefs_pos[compteur]) > 0: #Si le mot-clé est trouvé dans la phrase
             val = val + 1
-            if enreg.find(negation) > 0:
-                val = -1 * val
+            if enreg.find(negation) > 0: #Si le mot "pas" est trouvé
+                val = -1 * val #On inverse le résultat obtenu (si positif --> négatif, et vice versa)
+            if enreg.find(assurance) > 0:  # Si le mot "pas" est trouvé
+                val = 2 * val #On accentue le score dans le cas où l'utilisateur utilise "très" ou "trop" 
             val_inter = val_inter + val
-        compteur = compteur + 1
+        compteur = compteur + 1 #On passe au mot-clé suivant
     compteur = 0
     while compteur < len(liste_de_mots_clefs_neg):
         if enreg.find(liste_de_mots_clefs_neg[compteur]) > 0:
@@ -104,10 +93,7 @@ def find_connotation():  #utiliser variable globale pour last_val. vérif si glo
     # print("Connotation = ", last_val)
     return last_val
 
-def main():
-    global last_val
-
-    # Initialise screen
+def init_interface():
     pygame.init()
     screen = pygame.display.set_mode((750, 750))
     pygame.display.set_caption('Basic Pygame program')
@@ -126,17 +112,19 @@ def main():
     robot_face = pygame.transform.scale(robot_face, (750, 750))
     screen.blit(robot_face, (0, 0))
 
+def poser_les_questions():
+    global connotation_finale
+    for question in liste_questions:
+        play_sounds_with_gtts(question)
+        time.sleep(1)
+        connotation_finale = find_connotation()
+        connotation_finale += connotation_finale
+    return connotation_finale
+
+def main():
+    # global connotation_finale
+    init_interface()
     welcome_message()
-
-    # Listen thread
-    # listen_thread = threading.Thread(target=listen)
-    # listen_thread.start()
-
-    #find_connotation_thread = threading.Thread(target=find_connotation) #Créer un thread pour la fonction find_conno
-    #find_connotation_thread.start()
-
-    # Event loop
-
     while 1:
         time.sleep(0.1)
         pygame.display.flip()
@@ -147,15 +135,9 @@ def main():
             if event.type == pygame.KEYDOWN:
                 print("Question...")
                 poser_les_questions()
-                print("Connotation = ", last_val)
+                print("Connotation = ", connotation_finale)
                 goodbye_message()
 
-def poser_les_questions():
-
-    for question in liste_questions:
-        play_sounds_with_gtts(question)
-        time.sleep(1)
-        find_connotation()
 
 
 
@@ -163,6 +145,6 @@ if __name__ == '__main__':
     main()
 
 """
-Le programme de détection de voix ne capte généralement pas le premier mot prononcé
-La jauge de connotation ne calcule pas correctement
+Le programme de détection de voix ne capte généralement pas le premier mot prononcé -- Le programme ne reconnait pas bien les mots (problème micro?)
+La jauge de connotation ne calcule pas correctement -- Calcul faux (quelques mots clés oubliés?)
 """
